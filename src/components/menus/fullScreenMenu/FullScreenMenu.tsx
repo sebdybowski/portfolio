10070d442daae52c446b25ba2a2b6ecfe64d7649
sebdybowski <dybowski@int.pl>
2019-08-16 15:33:56 +0200

    Full screen menu beginnings

---
+++ src/components/menus/fullScreenMenu/FullScreenMenu.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export const FullScreenMenu = () => (
+    <nav className="full-screen-menu">
+        <div className="container">
+            <div className="row">
+                <div className="col">LOGO</div>
+                <div className="col">CLOSE</div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <ul className="nav flex-column">
+                        <li className="nav-item">
+                            <a className="nav-link active" href="#">Active</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <nav className="nav">
+                        <a className="nav-link active" href="#">Active</a>
+                        <a className="nav-link" href="#">Link</a>
+                        <a className="nav-link" href="#">Link</a>
+                        <a className="nav-link disabled" href="#" tabIndex={-1} aria-disabled="true">Disabled</a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </nav>
+);
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -11,7 +11,7 @@ export const Navbar = () => {
             <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">\
             <ul className="navbar-nav mr-auto">
                 <li className="nav-item active">
                     <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
